1ae7fe51a98c13cd0f4c05afc8856d14fca914d8
radityaseptian <radityaseptian1551@gmail.com>
2025-05-23 09:50:14 +0700

    build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_URL: 'http://127.0.0.1:3000/api',
+    NEXT_PUBLIC_API_URL: 'https://higo-technical-test-backend.vercel.app/api',
   },
 }
 
