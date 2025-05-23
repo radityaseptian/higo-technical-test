bff91b4d1ee6ea84c39709604e8a4c6cbd6ed813
radityaseptian <radityaseptian1551@gmail.com>
2025-05-23 09:54:32 +0700

    fix

--- frontend/src/app/(dashboard)/components/Table.tsx
+++ frontend/src/app/(dashboard)/components/Table.tsx
@@ -2,10 +2,12 @@ import { DataTable } from './table'
 import { columns } from './table/columns'
 
 async function getInitialData() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/summary?count=true&page=1&limit=10`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/summary?count=true&page=1&limit=10`, {
+    cache: 'no-store',
+  })
 
   if (!response.ok) {
-    return { data: [] }
+    return { data: [], count: 0 }
   }
 
   return response.json()
